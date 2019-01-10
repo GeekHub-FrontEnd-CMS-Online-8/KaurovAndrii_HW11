eccac77e6c6ce45ae33679a6a92b35022c62ef24
kaurovandrii96 <44208472+kaurovandrii96@users.noreply.github.com>
2019-01-10 23:00:55 +0200

    почти доработана главная страница

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 //slider
 $('.slide').slick();
+$('.slide__testi').slick({
+	dots: true,
+});
+
+//navigator
+$('.menu__link').mPageScroll2id({
+	speed : 100,
+});
+$('.nav__link').mPageScroll2id({
+	speed : 400,
+});
